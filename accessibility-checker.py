6e9109ee1150de82725c56b53c01b905d5afcba4
crypt0putin <crypt0putin@protonmail.com>
2020-11-13 11:01:39 +0300

    Done!

---
+++ accessibility-checker.py
@@ -0,0 +1,28 @@
+import requests, os, time
+from twilio.rest import Client
+from dotenv import load_dotenv
+load_dotenv()
+TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+r = requests.get(input('Введите полный адрес интернет ресурса для мониторинга доступности: '))
+
+def checker(msg):
+    message = client.messages.create(
+        body=msg,
+        from_='+12283357916',
+        to='+7xxxxxxxxxx',
+    )
+
+if r.ok == False:
+    while True:
+        msg = 'Привет! Похоже, что целевой ресурс в настоящее время недоступен.'
+        checker(msg)
+        time.sleep(60)
+
+else:
+    msg = 'Привет! Целевой ресурс в настоящее время работает исправно.'
+    checker(msg)
+
+print('Сообщение о доступности ресурса ресурса ужgitе в пути!')
